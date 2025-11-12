a2d1f14a723c10ce41b41dc3b0b5f4164f6ef3d7
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-11-12 08:04:39 -0300

    first commit

---
+++ src/test/java/dev/juan/EventClean/EventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juan.EventClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
