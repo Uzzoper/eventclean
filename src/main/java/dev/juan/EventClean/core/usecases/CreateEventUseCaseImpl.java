b22332535f5058a7dd1b26b75b0d4be7e56e7159
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-11-22 09:08:54 -0300

    refactor(core): inject EventGateway into use case and apply temporary @Service annotation

--- src/main/java/dev/juan/EventClean/core/usecases/CreateEventUseCaseImpl.java
+++ src/main/java/dev/juan/EventClean/core/usecases/CreateEventUseCaseImpl.java
@@ -1,8 +1,17 @@
 package dev.juan.EventClean.core.usecases;
 
 import dev.juan.EventClean.core.entities.Event;
+import dev.juan.EventClean.core.gateway.EventGateway;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateEventUseCaseImpl implements CreateEventUseCase {
+
+    private final EventGateway eventGateway;
+
+    public CreateEventUseCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
 
     @Override
     public Event execute(Event event) {
