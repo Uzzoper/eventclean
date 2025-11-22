15ae93efb87a2e53e863ae095628d6ecc49fc6dc
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-11-22 14:21:37 -0300

    refactor: remove Lombok from EventEntity and implement constructors and getters/setters manually

--- src/main/java/dev/juan/EventClean/infrastructure/persistence/EventEntity.java
+++ src/main/java/dev/juan/EventClean/infrastructure/persistence/EventEntity.java
@@ -10,9 +10,6 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "events")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class EventEntity {
 
     @Id
@@ -42,4 +39,100 @@ public class EventEntity {
     @Column(name = "type", nullable = false)
     @Enumerated(EnumType.STRING)
     private EventType type;
+
+    public EventEntity() {
+    }
+
+    public EventEntity(Long id, String name, String description, String identifier, LocalDateTime startDate, LocalDateTime endDate, String location, Integer capacity, String organizer, EventType type) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.identifier = identifier;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.capacity = capacity;
+        this.organizer = organizer;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
 }
